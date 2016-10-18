e8372397c1ddbb6a4d96dcd083a8d9ab008a7f5d
WWWietske <WWWietske@users.noreply.github.com>
2016-10-19 00:51:00 +0200

    Update counting.R

--- scripts/counting.R
+++ scripts/counting.R
@@ -3,7 +3,7 @@
 countarticles = function (x) {
   library(plyr)
   if("DATE" %in% colnames(x)){
-    count(x, "DATE")
+    temp <- count(x, "DATE")
     write.table(temp, "c:/data.txt", sep="\t")
   } else{
     print("not the right dataset")
