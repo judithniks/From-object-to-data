cc064b40665b3acb86c70f1bcc7829201352ccf1
WWWietske <WWWietske@users.noreply.github.com>
2016-10-19 00:58:53 +0200

    Add files via upload

--- scripts/counting.R
+++ scripts/counting.R
@@ -1,9 +1,9 @@
 # function to count the amount of articles written on each day
 
-countarticles = function (x) {
+countarticles = function (dataframe) {
   library(plyr)
-  if("DATE" %in% colnames(x)){
-    temp <- count(x, "DATE")
+  if("DATE" %in% colnames(dataframe)){
+    temp <- count(dataframe, "DATE")
     write.table(temp, "c:/data.txt", sep="\t")
   } else{
     print("not the right dataset")
