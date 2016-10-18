e277241a4669bbfd31eaa3153d1cea7aa36313dc
WWWietske <WWWietske@users.noreply.github.com>
2016-10-18 16:40:19 +0200

    added comments on what function does

--- scripts/merge.r
+++ scripts/merge.r
@@ -1,3 +1,5 @@
+# Script that takes 8 arguments and merges these 8 datasets together vertically
+
 mergedatasets = function (data1, data2, data3, data4, data5, data6, data7, data8) {
   foo <- list(data.frame(data1), data.frame(data2), data.frame(data3), data.frame(data4),
                 data.frame(data5), data.frame(data6), data.frame(data7), data.frame(data8))
